10ff701b4b929bef8699b44d7dc8dc750754f153
joannegton <wtg131998@gmail.com>
2024-03-21 00:17:16 -0300

    update

--- app/src/main/java/com/example/needdroneapp/ui/cadastros/CriarDroneActivity.java
+++ app/src/main/java/com/example/needdroneapp/ui/cadastros/CriarDroneActivity.java
@@ -1,26 +1,46 @@
-package com.example.needdroneapp.ui.cadastros;
+package com.example.needdroneapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.needdroneapp.R;
 
 public class CriarDroneActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_criar_drone);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        setupSpinner(R.id.spTipo);
+        setupSpinner(R.id.spQualidadeImagem);
+        setupSpinner(R.id.spAutonomia);
+        setupSpinner(R.id.spAreaCobertura);
+        setupSpinner(R.id.spStatus);
+    }
+
+    private void setupSpinner(int spinnerId) {
+        Spinner spinner = findViewById(spinnerId);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opcoes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String opcaoSelecionada = parent.getItemAtPosition(position).toString();
+                Toast.makeText(CriarDroneActivity.this, "Opção selecionada: " + opcaoSelecionada, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Ação a ser executada quando nenhuma opção é selecionada
+            }
         });
     }
 }
